eb82ee430df6cb4ed99bf4c215db46ad9375ba17
hule <hule@yiban.cn>
2019-10-29 11:21:54 +0800

    添加web在线版

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
+/*
+ * @Author: your name
+ * @Date: 2019-10-29 10:44:03
+ * @LastEditTime: 2019-10-29 11:21:14
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ */
 const gulp = require('gulp');
 
-function web () {
+function crx () {
   return gulp.src([
-    './web/manifest.json',
-    './web/scripts/**/*'
+    './web/manifest.json'
   ], {
     base: './web'
   })
+    .pipe(gulp.dest('./docs'))
     .pipe(gulp.dest('./build/web'));
 }
 
+function web () {
+  return gulp.src([
+    './build/web/**/*'
+  ], {
+    base: './build/web'
+  })
+    .pipe(gulp.dest('./docs'));
+}
+
 exports.web = web;
+exports.crx = crx;
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/huyue/apps/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/huyue/Documents/flutter/password"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_ROOT=D:\apps\flutter_windows_v1.9.1+hotfix.2-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\flutter_apps\password"
+export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/huyue/apps/flutter/bin/cache/artifacts/engine/ios"
+export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "FLUTTER_FRAMEWORK_DIR=D:\apps\flutter_windows_v1.9.1+hotfix.2-stable\flutter\bin\cache\artifacts\engine\ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
 export "FLUTTER_BUILD_NUMBER=1"
